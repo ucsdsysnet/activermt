a9394cae696727eac1ca86785ce031ef4794bf33
Rajdeep Das <r4das@ucsd.edu>
2023-08-08 11:01:58 -0700

    removed SDE-related scripts.

--- util/run_model.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-$SDE/run_tofino_model.sh -f $ACTIVEP4_SRC/config/portmap.json -p prototype
--- util/run_tests.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-TEST_SUITE=$1
-
-$SDE/run_p4_tests.sh --no-veth -f config/portmap.json -p prototype -t tests/$TEST_SUITE
