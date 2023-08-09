cfd35e170bc6aa01aa436fc52572a1989eae8e92
Rajdeep Das <r4das@ucsd.edu>
2023-08-08 17:50:22 -0700

    removed test P4.

--- clean.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-rm apps/cache/clients/udpc/kv_hits_misses*.csv
-rm clients/dpdk-client/active_tx_stats*.csv
-rm clients/dpdk-client/cache_stats*.csv
-rm clients/dpdk-client/dpdk_ap4_stats*.csv
